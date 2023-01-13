bcd973f7cc566f6a67ad547e597a15c64965c0ab
Terence Riss <115952375+TerenceRiss@users.noreply.github.com>
2023-01-13 14:34:15 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,88 +1,75 @@
 // APPEL DU FICHIER JSON VIA FETCH //
 
 fetch('movies.json')
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data){
-        // remplacement de la cont "movies" créée avant pour test par une nouvelle const "movies" prenend le data du json //
-        const movies = data
+.then(function(response) {
+    return response.json()
+})
+.then(function(data){
+    // remplacement de la cont "movies" créée avant pour test par une nouvelle const "movies" prenend le data du json //
+    const movies = data
+    
+    //  je recupere mon form 
+    const formElt = document.getElementById("search-form") 
 
-// je recupere l'input 
-const inputElt = document.getElementById("search-input")
+    // je recupere l'input 
+    const inputElt = document.getElementById("search-input")
+    
+    //  quand on tape dans l'input je recupere le mot 
+    inputElt.addEventListener('keyup', filterFilm)
+    
+    // appliquer cette fonction sur le submit 
+    formElt.addEventListener("submit", filterFilm)
 
-//  quand on tape dans l'input je recupere le mot 
+    //  cree une fonction qui filtre les films
+    function filterFilm(event) {
+        
+        const getValue = inputElt.value.toLowerCase();
+        console.log(getValue)
+        // je cherche tout les films dont le titre contient un I 
+        // toLowerCase regle le probleme de la majuscule ou minuscule pour l'utilisateur 
+        
+        const result2 = movies.filter(movie => movie.Title.toLowerCase().includes(getValue));
+        console.log(result2)
+        event.preventDefault();
+        
+        displayMovies(result2);
 
-inputElt.addEventListener('keyup', filterFilm)
-
-//  cree une fonction 
-function filterFilm(event) {
-
-    const getValue = inputElt.value.toLowerCase();
-    console.log(getValue)
-    // je cherche tout les films dont le titre contient un I 
-    // toLowerCase regle le probleme de la majuscule ou minuscule pour l'utilisateur 
-
-    const result2 = movies.filter(movie => movie.Title.toLowerCase().includes(getValue));
-    console.log(result2)
-    event.preventDefault();
-    // je recupere la classe movies
-    const moviesCardElt = document.querySelector('.movies')
-    moviesCardElt.innerHTML = '';
-    console.log(moviesCardElt)
-    var newHTML = "";
-    //  je fais une boucle sur le tableau 
-    for (let movieIndex = 0; movieIndex < result2.length; movieIndex++) {
-        console.log(result2[movieIndex])
-        //  j'ajouter dans le inner html de moviesCardElt le code suivant :
-        newHTML += `
-        <div class="movies_card">
-            <div class="movies_card_head">
-                <div class="movies_card_head_title">
-                    <h2 class="movies_card_head_title--name">${result2[movieIndex].Title}</h2>
-                    <span class="movies_card_head_title--line"></span>
-                    <p class="movies_card_head--realese">${result2[movieIndex].Release}</p>
-                </div>
-            </div>
-            <img class="movies_card--poster" src="${result2[movieIndex].Poster}" alt="">
-            <div class="movies_card_container">
-                <div class="movies_card_container_left">
-                    <ul class="movies_card_container_left_infos">
-                        <li class="movies_card_container_left_infos--director">by ${result2[movieIndex].Director}</li>`
-        for (let actorIndex = 0; actorIndex < result2[movieIndex].Actors.length; actorIndex++) {
-            newHTML += `
-                                <li class="movies_card_container_left_infos--actor">${result2[movieIndex].Actors[actorIndex]["First name"]} ${result2[movieIndex].Actors[actorIndex]["Last Name"]}</li>
-                            `    //${result2[movieIndex].Actors[actorIndex]["Nationality"]//
-        }
-        newHTML += `
-                    </ul>
-                </div>
-                <div class="movies_card_container_right">
-                    <ul class="movies_card_container_right_infos">
-                        <li class="movies_card_container_right_infos--duration">${result2[movieIndex].Duration} min</li>
-                        ${result2[movieIndex].Festivals.map(festival => `<li class="movies_card_container_right_infos--fest">${festival}</li>`).join('')}
-                    </ul>
-                </div>
-            </div>
-        </div>
-        `
     }
+        
+    // je recupere le bouton de tri 
+    const btnElt = document.getElementById("sort-input")
+    // appliquer cette fonction sur le submit 
+    btnElt.addEventListener("click", sortFilm)
 
-    moviesCardElt.innerHTML += newHTML;
-}
+    //  cree une fonction qui filtre les films
+    function sortFilm(event) {
 
-// appliquer cette fonction sur le submit 
-//  je recupere mon form 
-const formElt = document.getElementById("search-form")
+        // si le bouton possède la class "sort_asc" tri les films par ordre alphabétique
+        if(btnElt.classList.contains('sort_asc')) {
+            const sortedFilms = movies.sort((a, b) => a.Title.localeCompare(b.Title));
+            btnElt.classList.remove('sort_asc');
+            btnElt.classList.add('sort_desc');            
+            displayMovies(sortedFilms);
+        } else {
+            // sinon je trie les film dans l'ordre inverse de l'ordre alphabétique
+            const sortedFilms = movies.sort((a, b) => b.Title.localeCompare(a.Title));
+            btnElt.classList.remove('sort_desc');
+            btnElt.classList.add('sort_asc');            
+            displayMovies(sortedFilms);
+        }
 
-//  quand je soumet je recupere le mot 
-formElt.addEventListener("submit", filterFilm)
+    }
+    
+    
+    
+    // FONCTION POUR QUE LES FILMS SE CREENT DES L'OUVERTURE DE LA PAGE //
+    
+    displayMovies(movies);
+})
 
-
-// FONCTION POUR QUE LES FILMS SE CREENT DES L'OUVERTURE DE LA PAGE //
-
-createMovies();
-function createMovies(){
+// fonction qui ajoute toutes les cards dans le HTML
+function displayMovies(movies)
+{
     const moviesCardElt = document.querySelector('.movies')
     moviesCardElt.innerHTML = '';
     console.log(moviesCardElt)
@@ -91,41 +78,47 @@ function createMovies(){
     for (let movieIndex = 0; movieIndex < movies.length; movieIndex++) {
         console.log(movies[movieIndex])
         //  j'ajouter dans le inner html de moviesCardElt le code suivant :
-        newHTML += `
-        <div class="movies_card">
-            <div class="movies_card_head">
-                <div class="movies_card_head_title">
-                    <h2 class="movies_card_head_title--name">${movies[movieIndex].Title}</h2>
-                    <span class="movies_card_head_title--line"></span>
-                    <p class="movies_card_head--realese">${movies[movieIndex].Release}</p>
-                </div>
-            </div>
-            <img class="movies_card--poster" src="${movies[movieIndex].Poster}" alt="">
-            <div class="movies_card_container">
-                <div class="movies_card_container_left">
-                    <ul class="movies_card_container_left_infos">
-                        <li class="movies_card_container_left_infos--director">by ${movies[movieIndex].Director}</li>`
-        for (let actorIndex = 0; actorIndex < movies[movieIndex].Actors.length; actorIndex++) {
-            newHTML += `
-                                <li class="movies_card_container_left_infos--actor">${movies[movieIndex].Actors[actorIndex]["First name"]} ${movies[movieIndex].Actors[actorIndex]["Last Name"]}</li>
-                            `    //${movies[movieIndex].Actors[actorIndex]["Nationality"]//
-        }
-        newHTML += `
-                    </ul>
-                </div>
-                <div class="movies_card_container_right">
-                    <ul class="movies_card_container_right_infos">
-                        <li class="movies_card_container_right_infos--duration">${movies[movieIndex].Duration} min</li>
-                        ${movies[movieIndex].Festivals.map(festival => `<li class="movies_card_container_right_infos--fest">${festival}</li>`).join('')}
-                    </ul>
-                </div>
-            </div>
-        </div>
-        `
+        newHTML += displayCard(movies[movieIndex])
+        
     }
-
     moviesCardElt.innerHTML += newHTML;
 }
-})
 
+// fonction qui génère le HTML d'une card
+function displayCard(movie)
+{
+    let cardHTML = `
+    <div class="movies_card">
+    <div class="movies_card_head">
+    <div class="movies_card_head_title">
+    <h2 class="movies_card_head_title--name">${movie.Title}</h2>
+    <span class="movies_card_head_title--line"></span>
+    <p class="movies_card_head--realese">${movie.Release}</p>
+    </div>
+    </div>
+    <img class="movies_card--poster" src="${movie.Poster}" alt="">
+    <div class="movies_card_container">
+    <div class="movies_card_container_left">
+    <ul class="movies_card_container_left_infos">
+    <li class="movies_card_container_left_infos--director">by ${movie.Director}</li>`
+    for (let actorIndex = 0; actorIndex < movie.Actors.length; actorIndex++) {
+        cardHTML += `
+        <li class="movies_card_container_left_infos--actor">${movie.Actors[actorIndex]["First name"]} ${movie.Actors[actorIndex]["Last Name"]}</li>
+        `    //${movie.Actors[actorIndex]["Nationality"]//
+    }
+    cardHTML += `
+    </ul>
+    </div>
+    <div class="movies_card_container_right">
+    <ul class="movies_card_container_right_infos">
+    <li class="movies_card_container_right_infos--duration">${movie.Duration} min</li>
+    ${movie.Festivals.map(festival => `<li class="movies_card_container_right_infos--fest">${festival}</li>`).join('')}
+    </ul>
+    </div>
+    </div>
+    </div>
+    `;
+    
+    return cardHTML;
+}
 
